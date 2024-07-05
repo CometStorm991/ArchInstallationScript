510cad917c3cbe3060af83f5ea8a0199de42994a
CometStorm991 <cometstorm991@gmail.com>
2024-07-05 17:16:41 -0400

    Added keyboard remappings to GrayLaptop profile

--- Profiles/GrayLaptop/SetupXorg.sh
+++ Profiles/GrayLaptop/SetupXorg.sh
@@ -1,2 +1,4 @@
 sudo cp "$PROFILE_ASSETS_DIRECTORY/Udev/"* /etc/udev/rules.d/
 yay -S --needed input-remapper-git picom
+mkdir -p "$HOME/.config/"
+cp "$PROFILE_ASSETS_DIRECTORY/input-remapper-2/" "$HOME/.config/"
